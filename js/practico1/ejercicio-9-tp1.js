ba4d318f42e95640a4bbb893cc92e03bcd098888
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-05-23 16:07:07 -0300

    actualizado ej 9

--- js/practico1/ejercicio-9-tp1.js
+++ js/practico1/ejercicio-9-tp1.js
@@ -1,21 +1,7 @@
 let frase = prompt("ingresa una frase de 5 caracteres");
 
-if (frase.substring(0,1) === "a" || frase.substring(0,1) === "e" || frase.substring(0,1) === "i" || frase.substring(0,1) === "o" || frase.substring(0,1) === "u") {
-    document.write(frase.substring(0,1))
-}
-
-if (frase.substring(1,1) === "a" || frase.substring(1,1) === "e" || frase.substring(1,1) === "i" || frase.substring(1,1) === "o" || frase.substring(1,1) === "u") {
-    document.write(frase.substring(1,1))
-}
-
-if (frase.substring(2,1) === "a" || frase.substring(2,1) === "e" || frase.substring(2,1) === "i" || frase.substring(2,1) === "o" || frase.substring(2,1) === "u") {
-    document.write(frase.substring(2,1))
-}
-
-if (frase.substring(3,1) === "a" || frase.substring(3,1) === "e" || frase.substring(3,1) === "i" || frase.substring(3,1) === "o" || frase.substring(3,1) === "u") {
-    document.write(frase.substring(3,1))
-}
-
-if (frase.substring(4,1) === "a" || frase.substring(4,1) === "e" || frase.substring(4,1) === "i" || frase.substring(4,1) === "o" || frase.substring(4,1) === "u") {
-    document.write(frase.substring(4,1))
+for(let indice = 0; indice < frase.length; indice++) {
+    if (frase.charAt(indice) === "a" || frase.charAt(indice) === "e" || frase.charAt(indice) === "i" || frase.charAt(indice) === "o" || frase.charAt(indice) === "u") {
+    document.write(frase.charAt(indice))
+    }
 }
